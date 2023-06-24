b119fad4b329c1628e2409fa33c446335f6642e2
andy <lzjnhs@163.com>
2023-06-24 23:01:40 +0800

    Add files via upload

---
+++ util/common.cpp
@@ -0,0 +1,10 @@
+#include "common.h"
+#include "md5.h"
+#include "b64.h"
+
+char* password_encode(char* pwd)
+{
+    uint8_t md5_res[16];
+    md5String(pwd, md5_res);
+    return b64_encode(md5_res, 16);
+}
--- util/common.h
+++ util/common.h
@@ -1,1 +1,26 @@
+#ifndef DDMQTT_COMMON_H
+#define DDMQTT_COMMON_H
 
+#include "tlog.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+
+#define atomicSet(var, value)       __atomic_store_n(&(var), (value), __ATOMIC_SEQ_CST)
+#define atomicGet(var)              __atomic_load_n (&(var), __ATOMIC_SEQ_CST)
+#define atomicExchange(var, val)    __atomic_exchange_n(&(var), val, __ATOMIC_SEQ_CST)
+#define decrementAndGet(var, val)   __atomic_sub_fetch(&(var), val, __ATOMIC_SEQ_CST)
+#define incrementAndGet(var, val)   __atomic_add_fetch(&(var), val, __ATOMIC_SEQ_CST)
+
+#define mqtt_tid syscall(SYS_gettid)
+
+#define fatal_error(fmt, ...)           \
+do {                                    \
+    tlog_fatal(fmt, ##__VA_ARGS__);    \
+    tlog_exit();                        \
+    abort();                            \
+}while(0)
+
+char* password_encode(char* pwd);
+
+#endif
